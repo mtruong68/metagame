1c2cc942c0bc2ecefe6ce22509dc030fb434b250
mtruong68 <marytruong2020@u.northwestern.edu>
2017-11-30 22:50:22 -0600

    more options

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 var image1 = document.getElementById("image1");
 var poppyimage = document.getElementById("poppyimage");
+
+var poppy_text1 = document.getElementById("poppytext1");
+var poppy_text2 = document.getElementById("poppytext2");
+var poppy_text3 = document.getElementById("poppytext3");
+var poppylove3 = document.getElementById("poppylove3");
+var poppylove4 = document.getElementById("poppylove4");
+
 var poppyclicked = false;
+var poppycanclick = false;
 var poppycounter = 0;
 
 function poppy_f(){
@@ -22,6 +30,29 @@ function poppy_out(){
 }
 
 function poppy_love(){
-	poppyimage.src = "images/poppy3.png";
-	poppyclicked = true;
+	if (poppycanclick) {
+		++poppycounter;
+	}
+	if (poppycounter >= 10){
+		poppyimage.src = "images/poppy3.png";
+		poppyclicked = true;
+		poppylove4.style.display = "block";
+	}
 }
+
+function poppy_text2_f() {
+	poppytext1.style.display="none";
+	poppytext2.style.display="inline";
+}
+
+function poppy_text3_f() {
+	poppytext2.style.display="none";
+	poppytext3.style.display="inline";
+}
+
+function poppy_text4_f() {
+	poppylove3.style.display="inline";
+	poppycanclick = true;
+}
+
+
