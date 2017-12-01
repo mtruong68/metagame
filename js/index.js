4d45e671802faf58c19ddc927d916d8c6f5bdf30
mtruong68 <marytruong2020@u.northwestern.edu>
2017-11-30 21:47:41 -0600

    index moved to main directory

--- js/index.js
+++ js/index.js
@@ -9,19 +9,19 @@ function poppy_f(){
 
 function poppy_wave(){
 	if (!poppyclicked){
-		poppyimage.src = "../images/poppy2.png";
+		poppyimage.src = "images/poppy2.png";
 	}
 }
 
 function poppy_out(){
 	if (poppyclicked){
-		poppyimage.src = "../images/poppy3.png";
+		poppyimage.src = "images/poppy3.png";
 	} else {
-		poppyimage.src = "../images/poppy1.png";
+		poppyimage.src = "images/poppy1.png";
 	}
 }
 
 function poppy_love(){
-	poppyimage.src = "../images/poppy3.png";
+	poppyimage.src = "images/poppy3.png";
 	poppyclicked = true;
 }
