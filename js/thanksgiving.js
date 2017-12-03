c037ae52ae2c67da23f9d967c3def3ac4a6c14c0
mtruong68 <marytruong2020@u.northwestern.edu>
2017-12-02 20:25:27 -0600

    lots of changes to pages

---
+++ js/thanksgiving.js
@@ -0,0 +1,21 @@
+var text1 = document.getElementById("text1");
+var pop = document.getElementById("pop");
+var poppyimg = document.getElementById("poppy");
+
+
+function friend() {
+	text1.style.display = "block";
+	pop.style.display = "block";
+} 
+
+function poppy_wave(){
+	poppy.src = "../images/poppy2.png";
+}
+
+function poppy_out(){
+
+	poppy.src = "../images/poppy1.png";
+}
+
+function poppy_love(){
+}
